db15b6efcb90db0bb3501f38bdb7e635fde8c871
Aaron Peter Viju <aaron.evolvier@gmail.com>
2025-06-05 12:27:39 +0530

    GEN 17 commit

--- hello_world/express-backend/index.js
+++ hello_world/express-backend/index.js
@@ -4,10 +4,27 @@ const PORT = process.env.PORT || 5000;
 
 
 app.get('/api/test', (req, res) => {
-  res.json({ message: 'Hello from the test route!' });
+  res.json(articles);
 });
 
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+const articles =[
+  {
+    id: 1,
+    title: "Getting Started with Express.js",
+    content: "Express.js is  minimal and flexible Node.js web application framework that provides a robust set of features for web applications."
+  },
+    {
+    id: 2,
+    title: "GET",
+    content: "Test GET."
+  },  {
+    id: 3,
+    title: "POST",
+    content: "Test POST."
+  }
+];
